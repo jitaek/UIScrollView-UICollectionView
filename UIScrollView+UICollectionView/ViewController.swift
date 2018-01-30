2faa1f27d0e84de9f03f2a64cb53a84b092fa3cd
jitae <jitaekim93@gmail.com>
2018-01-29 21:23:33 -0800

    Initial Commit

---
+++ UIScrollView+UICollectionView/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  UIScrollView+UICollectionView
+//
+//  Created by Jitae Kim on 1/29/18.
+//  Copyright © 2018 Jitae Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        collectionView.isScrollEnabled = false
+    }
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 60
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        
+        cell.backgroundColor = indexPath.row % 2 == 0 ? UIColor.lightGray : UIColor.red
+        
+        return cell
+    }
+
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        
+        if scrollView == self.scrollView {
+            collectionView.isScrollEnabled = (self.scrollView.contentOffset.y >= 200)
+        }
+        
+        if scrollView == self.collectionView {
+            collectionView.isScrollEnabled = (collectionView.contentOffset.y > 0)
+        }
+    }
+
+}
+
